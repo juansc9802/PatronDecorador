0ed61e11f8604baa2e5762ff217b253550bf6087
juansc9802 <juansc9802@gmail.com>
2023-04-26 11:16:15 -0500

    Agregación final

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 public class Main {
@@ -5,17 +7,33 @@ public class Main {
         Scanner lector = new Scanner(System.in);
         System.out.println("Bienvenido");
 
+        Calendar fechaAct = new GregorianCalendar(2022, Calendar.APRIL, 24);
+        Calendar fechaEnt = new GregorianCalendar(2022, Calendar.APRIL, 28);
         ConcreteComponent compOG = new ConcreteComponent();
         ConcreteDecorator_1 compEmail = new ConcreteDecorator_1();
+        ConcreteDecorator_2 compMsg = new ConcreteDecorator_2();
 
-        //compOG.notif();
-        System.out.println("Mensaje nuevo");
-        System.out.println("------------------------------------------------------------");
 
-        compEmail.setComponent(compOG);
-        compEmail.notif();
+        String Menu = """ 
+                        Elige una opción:
+                        1. Email
+                        2. Mensaje de texto""";
+        System.out.println(Menu);
 
-        System.out.println("------------------------------------------------------------");
+        int resp = lector.nextInt();
+        if (resp == 1){
+            compEmail.setComponent(compOG);
+            System.out.println("------------------------------------------------------------");
+                if (fechaAct.get(Calendar.MONTH) == fechaEnt.get(Calendar.MONTH) && fechaAct.get(Calendar.DAY_OF_MONTH) == ((fechaEnt.get(Calendar.DAY_OF_MONTH))-1)){
+                compEmail.notif();
+            }
+        }
+        else if(resp==2) {compMsg.setComponent(compOG);
+            System.out.println("------------------------------------------------------------");
+            if (fechaAct.get(Calendar.MONTH) == fechaEnt.get(Calendar.MONTH) && fechaAct.get(Calendar.DAY_OF_MONTH) == ((fechaEnt.get(Calendar.DAY_OF_MONTH))-1)){
+                compMsg.notif();
+            }
+        }
 
     }
 }
